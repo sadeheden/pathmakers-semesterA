7fab382bc79934718b9185db6808de6194312acc
sadeheden <sadeheden@gmail.com>
2025-02-17 14:31:06 +0200

    fix

--- server/services/cities/cities.router.js
+++ server/services/cities/cities.router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getCities, addCity } from '../controllers/cities.controller.js';
+import { getCities, addCity } from '../cities.controller.js';
 
 const router = express.Router();
 
