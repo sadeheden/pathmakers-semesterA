645d016db805b87e5ebb854f95e8d403b8f76a7d
mayhalaf <155309787+mayhalaf@users.noreply.github.com>
2025-02-19 00:34:06 +0200

    n

--- server/router.js
+++ server/router.js
@@ -2,7 +2,7 @@ import Router from 'express';
 import authRouter from './services/auth/auth.router.js';
 import uploadRouter from './services/upload/upload.router.js';
 import citiesRouter from './services/cities/cities.router.js';
-import attractionsRouter from './services/attraction/att.router.js'; // הוסף ייבוא
+
 
 const router = Router();
 
@@ -14,6 +14,6 @@ router.get('/', (req, res) => {
 router.use('/auth', authRouter);
 router.use('/upload', uploadRouter);
 router.use('/cities', citiesRouter); 
-router.use('/attractions', attractionsRouter);
+
 export default router;
 
