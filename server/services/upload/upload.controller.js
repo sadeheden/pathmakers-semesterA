c7a8f924ad7891d0a74070c839f8cb41a00150fe
mayhalaf <155309787+mayhalaf@users.noreply.github.com>
2025-02-19 16:46:26 +0200

    n

--- server/services/upload/upload.controller.js
+++ server/services/upload/upload.controller.js
@@ -1,15 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();  // טוען את משתני הסביבה
+
 import { v2 as cloudinary } from 'cloudinary';
 
 console.log("✅ Loading Cloudinary ENV Variables:", {
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
     CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET ? "Exists ✅" : "❌ MISSING"
 });
 
+// הגדרת Cloudinary עם שם הענן
 cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
 });
-
 
 export async function uploadToCloudinary(req, res) {
     try {
