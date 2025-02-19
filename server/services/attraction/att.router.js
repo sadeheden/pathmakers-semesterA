912b878888065aeddb3282cf620614eabf214315
sadeheden <sadeheden@gmail.com>
2025-02-19 10:59:40 +0200

    .

--- server/services/attraction/att.router.js
+++ server/services/attraction/att.router.js
@@ -3,20 +3,6 @@ import { getAttractionsByCity } from "./att.controller.js";
 
 const router = express.Router();
 
-router.get("/:city", async (req, res) => {
-  const { city } = req.params;
-  try {
-    const attractions = await getAttractionsByCity(city);
-
-    if (!attractions.length) {
-      return res.status(404).json({ error: `No attractions found for city: ${city}` });
-    }
-
-    res.json(attractions);
-  } catch (error) {
-    console.error("Error fetching attractions:", error);
-    res.status(500).json({ error: "Server error" });
-  }
-});
+router.get('/',getAttractionsByCity);
 
 export default router;
