355a98adf7093e673607188c5fd5ffbf2d09668d
mayhalaf <155309787+mayhalaf@users.noreply.github.com>
2025-02-23 14:22:13 +0200

    צ

--- client/cypress/component/header.jsx
+++ client/cypress/component/header.jsx
@@ -1,9 +1,9 @@
-import { mount } from "@cypress/react";
+import { mount } from '@cypress/react18';
 import Header from "../../src/components/Header";
 
 describe('Header test', () => {
     it('should present the Header and the text', () => {
-        mount(<Header text="main" />);
-        cy.get('button').should('have.text', 'main');
+        mount(<Header text="Main" />);
+        cy.get('button').should('have.text', 'Main');
     })
 });
