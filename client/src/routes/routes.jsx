c3caf1f8ec68e582d582369a71e405f7c9ed7df7
sadeheden <sadeheden@gmail.com>
2025-04-17 15:39:57 +0300

    realchat

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
-
+ 
 // Import all pages directly
 import HomePage from "../pages/HomePage";
 import AboutPage from "../pages/AboutPage";
@@ -12,7 +12,10 @@ import DownloadApp from "../pages/DownloadApp";
 import Signup from "../pages/Signup";
 import Main from "../pages/Main";
 import NotFound from "../pages/NotFound"; // Custom 404 Page
-
+import Manager from "../pages/Manager";
+import RealChat from "../pages/RealChat";
+ 
+ 
 export const Router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/about", element: <AboutPage /> },
@@ -23,5 +26,9 @@ export const Router = createBrowserRouter([
   { path: "/video", element: <Video /> },
   { path: "/downloadApp", element: <DownloadApp /> },
   { path: "/signup", element: <Signup /> },
+  { path: "/manager", element: <Manager /> },
+  { path: "/realChat", element: <RealChat /> },
+ 
   { path: "*", element: <NotFound /> }, // Handles unmatched routes
 ]);
+ 
