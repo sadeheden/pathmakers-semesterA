b9bb03b247e413c7d6443fbf9cc5710b6bfc47ab
sadeheden <sadeheden@gmail.com>
2025-04-17 15:39:57 +0300

    realchat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,12 +10,14 @@ import DownloadApp from "./pages/DownloadApp";
 import Video from "./pages/Video";
 import AuthForm from "./pages/AuthForm"; // ✅ Use AuthForm for both login & signup
 import Main from "./pages/Main";
-
+import Manager from "./pages/Manager";
+import RealChat from "./pages/RealChat";
+ 
 function App() {
   return (
     <Router>
       <Header />  {/* Header component */}
-      
+     
       <div className="content">
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -24,16 +26,17 @@ function App() {
           <Route path="/signup" element={<AuthForm isLogin={false} />} /> {/* ✅ Add signup route */}
           <Route path="/personal-area" element={<PersonalArea />} />
           <Route path="/chat" element={<Chat />} />
+          <Route path="/manager" element={<Manager/>} />
           <Route path="/downloadApp" element={<DownloadApp />} />
           <Route path="/video" element={<Video />} />
           <Route path="/main" element={<Main />} />
-        
+          <Route path="/realChat" element={<RealChat />} />
         </Routes>
       </div>
-
+ 
       <Footer />  {/* Footer component */}
     </Router>
   );
 }
-
+ 
 export default App;
